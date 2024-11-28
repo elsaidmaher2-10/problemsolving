21fdddc1630723e733de4edee99fd412cf0004cb
elsaidmaher2-10 <elhakeemelkhodary@gmail.com>
2024-11-28 14:20:54 +0200

    Add files via upload

---
+++ Problem #5 reversed number.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int readnumber(string message) {
+    int number;
+    do {
+        cout << message << endl;
+        cin >> number;
+    } while (number <= 0);
+        return number;
+}
+void reversedint() {
+    int number = readnumber("Enter postivenumber");
+    int  revers = 0;
+    do {
+        revers = number % 10;
+        cout << revers << endl; 
+        number = number / 10;
+
+    } while (number != 0);
+}
+
+int main()
+{
+    reversedint();
+}
+
